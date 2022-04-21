2b81d4664f865894cc58518fd2ef5a9090ca9706
Vasko Bozinovski <vbozinovski16@gmail.com>
2022-04-21 21:23:59 +0200

    first commit

---
+++ src/main/java/mk/ukim/finki/emt_193005/model/Author.java
@@ -0,0 +1,30 @@
+package mk.ukim.finki.emt_193005.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String name;
+
+    String surname;
+
+    @ManyToOne
+    Country country;
+
+    public Author() {
+    }
+
+    public Author(String name, String surname, Country country) {
+        this.name = name;
+        this.surname = surname;
+        this.country = country;
+    }
+}
---
+++ src/main/java/mk/ukim/finki/emt_193005/model/enumerations/Category.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.emt_193005.model.enumerations;
+
+public enum Category {
+    NOVEL("NOVEL"),
+    THRILLER("THRILLER"),
+    HISTORY("HISTORY"),
+    FANTASY("FANTASY"),
+    BIOGRAPHY("BIOGRAPHY"),
+    CLASSICS("CLASSICS"),
+    DRAMA("DRAMA");
+
+    private final String displayValue;
+
+    Category(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+}
